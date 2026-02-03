782e498c2d11a1ee53ce15ebc54e415d7eaa2f73
formateur-glodie-tshimini <contact@tshimini.fr>
2026-02-03 14:29:27 +0100

    feat: workshop 2.4 2/2

--- ateliers/corrections/2.4/reponses_join_on.sql
+++ ateliers/corrections/2.4/reponses_join_on.sql
@@ -1,10 +1,11 @@
 -- 1. Affichez le nom, le prénom, la fonction et le salaire des employés qui ont un salaire compris entre 2500 et 3500.
+-- Prefixe des colonnes par le nom de la table ici purement altruiste
 SELECT employes.NOM,
   employes.PRENOM,
   employes.FONCTION
 FROM employes
 WHERE employes.SALAIRE >= 2500
-  AND employes.SALAIRE <= 3500;
+AND employes.SALAIRE <= 3500;
 -- 2. Affichez le nom du produit, le nom du fournisseur, le nom de la catégorie et 
 -- les quantités de produits qui ne sont pas d’une des catégories 1,3,5 et 7.
 SELECT P.NOM_PRODUIT,
@@ -15,8 +16,8 @@ FROM produits P,
   fournisseurs F,
   categories C
 WHERE C.CODE_CATEGORIE = P.CODE_CATEGORIE
-  AND F.NO_FOURNISSEUR = P.NO_FOURNISSEUR
-  AND C.CODE_CATEGORIE NOT IN ('1', '3', '5', '7');
+AND F.NO_FOURNISSEUR = P.NO_FOURNISSEUR
+AND C.CODE_CATEGORIE NOT IN ('1', '3', '5', '7');
 -- 3. Affichez le nom du produit, le nom du fournisseur, le nom de la catégorie et les quantités des produits qui ont
 -- un numéro de fournisseur entre 1 et 3
 -- ou un code catégorie entre 1 et 3
@@ -40,3 +41,30 @@ WHERE z.code_categorie = x.code_categorie
     x.quantite LIKE '%bo_tes%'
     OR x.quantite LIKE '%carton%'
   );
+-- 4. Écrivez la requête qui permet d’afficher le nom des employés qui ont effectué au moins une vente pour un client parisien.
+SELECT e.NOM,
+  cl.SOCIETE,
+  cl.VILLE
+FROM employes e
+JOIN commandes cmd
+ON e.NO_EMPLOYE = cmd.NO_EMPLOYE
+JOIN clients cl
+ON cmd.CODE_CLIENT = cl.CODE_CLIENT
+WHERE VILLE = 'Paris';
+-- 5. Affichez le nom des produits et le nom des fournisseurs pour les produits des catégories 1,4 et 7.
+SELECT NOM_PRODUIT,
+  SOCIETE
+FROM produits p
+JOIN fournisseurs f
+ON p.NO_FOURNISSEUR = f.NO_FOURNISSEUR
+WHERE CODE_CATEGORIE IN (1, 4, 7);
+-- 6. Affichez le nom des employés ainsi que le nom de leur supérieur hiérarchique.
+SELECT s.NOM AS nom_salarie,
+  s.PRENOM AS salarie_prenom,
+  s.REND_COMPTE numero_du_chef,
+  s.NO_EMPLOYE AS numero_employe_salarie,
+  chef.NOM nom_du_chef,
+  chef.PRENOM prenom_du_chef
+FROM employes s
+JOIN employes chef
+ON chef.no_employe = s.REND_COMPTE;
---
+++ ateliers/corrections/2.4/reponses_where.sql
@@ -0,0 +1,79 @@
+-- 1. Affichez le nom, le prénom, la fonction et le salaire des employés qui ont un salaire compris entre 2500 et 3500.
+-- Prefixe des colonnes par le nom de la table ici purement altruiste
+SELECT employes.NOM,
+  employes.PRENOM,
+  employes.FONCTION
+FROM employes
+WHERE employes.SALAIRE >= 2500
+AND employes.SALAIRE <= 3500;
+-- 2. Affichez le nom du produit, le nom du fournisseur, le nom de la catégorie et 
+-- les quantités de produits qui ne sont pas d’une des catégories 1,3,5 et 7.
+SELECT P.NOM_PRODUIT,
+  F.SOCIETE,
+  C.NOM_CATEGORIE,
+  P.QUANTITE
+FROM produits P,
+  fournisseurs F,
+  categories C
+WHERE C.CODE_CATEGORIE = P.CODE_CATEGORIE
+AND F.NO_FOURNISSEUR = P.NO_FOURNISSEUR
+AND C.CODE_CATEGORIE NOT IN ('1', '3', '5', '7');
+-- 3. Affichez le nom du produit, le nom du fournisseur, le nom de la catégorie et les quantités des produits qui ont
+-- un numéro de fournisseur entre 1 et 3
+-- ou un code catégorie entre 1 et 3
+-- et pour lesquelles les quantités sont données en boîte(s) ou en carton(s).
+SELECT x.NOM_PRODUIT,
+  y.societe,
+  z.nom_categorie,
+  x.quantite,
+  y.no_fournisseur,
+  z.code_categorie
+FROM produits x,
+  fournisseurs y,
+  categories z
+WHERE z.code_categorie = x.code_categorie
+  AND y.no_fournisseur = x.no_fournisseur
+  AND (
+    CAST(y.no_fournisseur AS SIGNED) <= 3
+    OR CAST(z.code_categorie AS SIGNED) <= 3
+  )
+  AND (
+    x.quantite LIKE '%bo_tes%'
+    OR x.quantite LIKE '%carton%'
+  );
+-- 4. Écrivez la requête qui permet d’afficher le nom des employés qui ont effectué au moins une vente pour un client parisien.
+SELECT e.NOM,
+  cl.SOCIETE,
+  cl.VILLE
+FROM employes e,
+  commandes cmd,
+  clients cl
+WHERE e.NO_EMPLOYE = cmd.NO_EMPLOYEé
+  AND cmd.CODE_CLIENT = cl.CODE_CLIENT
+  AND VILLE = 'Paris';
+-- 5. Affichez le nom des produits et le nom des fournisseurs pour les produits des catégories 1,4 et 7.
+SELECT NOM_PRODUIT,
+  SOCIETE
+FROM produits p,
+  fournisseurs f
+WHERE p.NO_FOURNISSEUR = f.NO_FOURNISSEUR
+  AND CODE_CATEGORIE IN (1, 4, 7);
+-- 6. Affichez le nom des employés ainsi que le nom de leur supérieur hiérarchique.
+SELECT s.NOM AS nom_salarie,
+  s.PRENOM AS salarie_prenom,
+  s.REND_COMPTE numero_du_chef,
+  s.NO_EMPLOYE AS numero_employe_salarie,
+  chef.NOM nom_du_chef,
+  chef.PRENOM prenom_du_chef
+FROM employes s,
+  employes chef
+WHERE chef.no_employe = s.REND_COMPTE;
+--- 6. Avec Jointure
+SELECT s.NOM AS nom_salarie,
+  s.PRENOM AS salarie_prenom,
+  s.REND_COMPTE numero_du_chef,
+  s.NO_EMPLOYE AS numero_employe_salarie,
+  chef.NOM nom_du_chef,
+  chef.PRENOM prenom_du_chef
+FROM employes s, employes chef
+WHERE chef.no_employe = s.REND_COMPTE;
