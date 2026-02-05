16698f56eeead95b113dbfa175f66043dd865c85
formateur-glodie-tshimini <contact@tshimini.fr>
2026-02-05 15:54:14 +0100

    fix: comments

--- ateliers/corrections/7/reponses.sql
+++ ateliers/corrections/7/reponses.sql
@@ -1,45 +1,51 @@
--- 1. Créer une table pays avec 2 champs : code
-CREATE TABLE pays (code_pays CHAR(4) NOT NULL PRIMARY KEY, nom_pays VARCHAR(40));
----     pays (4 caractères, clé primaire), nom pays (40
----     caractères maximum)
---- 2. Ajouter une colonne courriel (60 caractères
----     possibles) à la table CLIENTS. Puis la modifier
----     pour passer à 75 caractères. Pour finir,
----     supprimer cette colonne.
---- 3. Créez une vue qui affiche le nom de la société,
----   l'adresse, le téléphone et la ville des clients qui habitent en province.
-
-1. 
-
-2. Ajout de la colonne
-ALTER TABLE CLIENTS ADD (courriel VARCHAR(75));
-
-Modification de la colonne
-Oracle et MySQL
-ALTER TABLE CLIENTS MODIFY (COURRIEL VARCHAR(60));
-
-SQL Server et Postgre
-ALTER TABLE CLIENTS ALTER COLUMN (courriel VARCHAR2(60));
-
-Suppression de la colonne
+-- 1. Créer une table pays avec 2 champs : code pays (4 caractères, clé primaire), nom pays (40 caractères maximum)
+CREATE TABLE pays (
+  code_pays CHAR(4) NOT NULL PRIMARY KEY,
+  nom_pays VARCHAR(40)
+);
+-- 2. Ajouter une colonne courriel (60 caractères possibles) à la table CLIENTS.
+---   Puis la modifier pour passer à 75 caractères.
+---   Pour finir, supprimer cette colonne.
+ALTER TABLE CLIENTS
+ADD (courriel VARCHAR(60));
+-- Oracle et MySQL
+ALTER TABLE CLIENTS
+MODIFY (COURRIEL VARCHAR(75));
+-- SQL Server et PostgreSQL
+ALTER TABLE CLIENTS ALTER COLUMN (courriel VARCHAR2(75));
 ALTER TABLE CLIENTS DROP COLUMN courriel;
-
-3. CREATE VIEW V_CLIENTS_PROVINCE AS
-SELECT SOCIETE, ADRESSE, TELEPHONE, VILLE
-FROM CLIENTS WHERE PAYS="France"
-AND CODE_POSTAL NOT LIKE "75%" AND CODE_POSTAL NOT LIKE "77%" 
-AND CODE_POSTAL NOT LIKE "78%" AND CODE_POSTAL NOT LIKE "91%" 
-AND CODE_POSTAL NOT LIKE "92%" AND CODE_POSTAL NOT LIKE "93%" 
-AND CODE_POSTAL NOT LIKE "94%" AND CODE_POSTAL NOT LIKE "95%" ;
-
-Ou Oracle et Postgre :
+-- 3. Créez une vue qui affiche le nom de la société,
+---   l'adresse, le téléphone et la ville des clients qui habitent en province.
 CREATE VIEW V_CLIENTS_PROVINCE AS
-SELECT SOCIETE, ADRESSE, TELEPHONE, VILLE
-FROM CLIENTS WHERE PAYS="France"
-AND SUBSTR(CODE_POSTAL,1,2) NOT IN ("75","77","78","91","92","93","94","95");
-
-SQL Server et MySQL :
+SELECT SOCIETE,
+  ADRESSE,
+  TELEPHONE,
+  VILLE
+FROM CLIENTS
+WHERE PAYS = "France"
+  AND CODE_POSTAL NOT LIKE "75%"
+  AND CODE_POSTAL NOT LIKE "77%"
+  AND CODE_POSTAL NOT LIKE "78%"
+  AND CODE_POSTAL NOT LIKE "91%"
+  AND CODE_POSTAL NOT LIKE "92%"
+  AND CODE_POSTAL NOT LIKE "93%"
+  AND CODE_POSTAL NOT LIKE "94%"
+  AND CODE_POSTAL NOT LIKE "95%";
+-- Oracle et PostgreSQL :
 CREATE VIEW V_CLIENTS_PROVINCE AS
-SELECT SOCIETE, ADRESSE, TELEPHONE, VILLE
-FROM CLIENTS WHERE PAYS="France"
-AND SUBSTRING(CODE_POSTAL,1,2) NOT IN ("75","77","78","91","92","93","94","95");
+SELECT SOCIETE,
+  ADRESSE,
+  TELEPHONE,
+  VILLE
+FROM CLIENTS
+WHERE PAYS = "France"
+  AND SUBSTR(CODE_POSTAL, 1, 2) NOT IN ("75", "77", "78", "91", "92", "93", "94", "95");
+-- SQL Server et MySQL :
+CREATE VIEW V_CLIENTS_PROVINCE AS
+SELECT SOCIETE,
+  ADRESSE,
+  TELEPHONE,
+  VILLE
+FROM CLIENTS
+WHERE PAYS = "France"
+  AND SUBSTRING(CODE_POSTAL, 1, 2) NOT IN ("75", "77", "78", "91", "92", "93", "94", "95");
